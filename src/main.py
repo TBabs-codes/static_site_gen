ac89a8465dca76e1bb8156639513ee7f136ee363
TBabs-codes <tommy.f.babcock@gmail.com>
2024-08-24 21:23:15 -0700

    Main.py update: static copies to public

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 import os
+import shutil
 
-print(os.path.exists("./static"))
+shutil.rmtree("./public") #deletes entire public directory.
 
-print(os.listdir())
+os.mkdir("./public") #creates public directory.
 
-# print(os.path.join("./static"))
+def copyfiles(src_path, dst_path): #copyfiles function copies files from src_path directory into dst_path directory preserving the orginal file tree.
+
+    static_file_tree = os.listdir(f"{src_path}")
+
+    for obj in static_file_tree:
+        if os.path.isfile(f"{src_path}/{obj}"):
+            shutil.copy(f"{src_path}/{obj}", f"{dst_path}")
+        else:
+            os.mkdir(f"{dst_path}/{obj}")
+            copyfiles(f"{src_path}/{obj}", f"{dst_path}/{obj}")
+
+copyfiles("./static", "./public") #This code is readable. No comment.
